a6483c709897478a9dcd71177c9403587aeb337d
rikoriswandha <rikorisw@gmail.com>
2018-03-16 21:25:30 +0800

    Added delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,5 +31,11 @@ class PostsController < ApplicationController
     redirect_to(post_path(@post))
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
 end
   
