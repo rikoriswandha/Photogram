4c67957e6a0f542330fea6dbb30a57c5998e62cf
rikoriswandha <rikorisw@gmail.com>
2018-03-17 19:28:22 +0800

    Associating posts with their own users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -9,15 +10,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      flash[:alert] = "Your new post couldn't be created! Please check the form."
       render :new
     end
   end
