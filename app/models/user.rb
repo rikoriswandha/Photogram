8397222d03f7e3a2a04aca1ff005d9f332b6b8a0
rikoriswandha <rikorisw@gmail.com>
2018-03-18 22:54:57 +0800

    Added some registration validation for email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: 'only letters and numbers allowed' }
+  validates_uniqueness_of :user_name, message: 'username has already taken'
+  validates_uniqueness_of :email, message: 'email has already registered'
+  validates_uniqueness_of :user_name, case_sensitive: false, message: 'only lowercase letters allowed'
   has_many :posts, dependent: :destroy
   has_attached_file :avatar, styles: { medium: '152x152#' }
   validates_attachment_content_type :avatar, content_type: /\Aimage/
