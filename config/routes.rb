717c15561c6bacf31d52bcf7523cccaae6bfc32d
rikoriswandha <rikorisw@gmail.com>
2018-03-18 19:55:07 +0800

    Added user edit profile and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'profiles/show'
+
 devise_for :users, :controllers => { registrations: 'registrations' }
 resources :posts do
   resources :comments
 end
 
 root 'posts#index'
+get ':user_name', to: 'profiles#show', as: :profile
+get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+patch ':user_name/edit', to: 'profiles#update', as: :update_profile
 
 end
